f1bd1d7cbb1386ec8b2f9b07024511313d6531cc
AbuHamse <dirie.a99@gmail.com>
2026-03-26 12:18:09 -0400

    New pages Added

---
+++ client/src/pages/FictionGenrePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FictionGenrePage = () => {
+  return (
+    <div>FictionGenrePage</div>
+  )
+}
+
+export default FictionGenrePage
---
+++ client/src/pages/NonFictionGenrePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NonFictionGenrePage = () => {
+  return (
+    <div>NonFictionGenrePage</div>
+  )
+}
+
+export default NonFictionGenrePage
